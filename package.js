b5ab20e434e2265654279273df28318b08cfdc32
guncebektas <guncebektas@gmail.com>
2015-11-22 18:38:05 +0200

    added api.imply

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'guncebektas:autocms',
-  version: '0.0.9',
+  version: '0.0.10',
   // Brief, one-line summary of the package.
   summary: 'AutoCms is a simple solution to manage contents',
   // URL to the Git repository containing the source code for this package.
@@ -12,6 +12,17 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
+
+  api.imply([
+    'underscorestring:underscore.string',
+    'kadira:flow-router@2.7.0',
+    'kadira:blaze-layout@2.2.0',
+    'arillo:flow-router-helpers@0.4.6',
+    'aldeed:collection2@2.5.0',
+    'aldeed:autoform@5.7.1',
+    'aldeed:simple-schema@1.3.3',
+    'aldeed:delete-button@1.0.0'
+  ]);
 
   // set dependencies
   api.use('ecmascript');
