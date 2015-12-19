5b624304b43c192bb4f25b39ce7f0c2e00f6a4b6
guncebektas <guncebektas@gmail.com>
2015-12-19 23:21:22 +0200

    version change

--- package.js
+++ package.js
@@ -10,7 +10,7 @@
 */
 Package.describe({
   name: 'guncebektas:autocms',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'AutoCms is a simple solution to manage contents',
   // URL to the Git repository containing the source code for this package.
@@ -66,7 +66,6 @@ Package.onUse(function(api) {
 
   // add files
   api.addFiles([
-    'routes.js',
     'templates/fileUploadSystem.html',
     'templates/autoCms.html',
     'methods.js',
