a1ebde1ec5e15e2f707ece74daef3c00d38eeccd
guncebektas <guncebektas@gmail.com>
2016-01-25 18:16:45 +0200

    correction
    solved bug on firefox

--- events.js
+++ events.js
@@ -1,6 +1,6 @@
 Template.autoCms.events({
   // before remove action for autoTable
-  'click .remove' : function() {
+  'click .remove' : function(event) {
   	if (confirm('Are you sure to delete?')) {
 		  window[collection].remove($(event.target).data('id'));        
 		}
