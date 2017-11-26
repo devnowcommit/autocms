2c975bdb79bf1b95fd166114859f3569b71b872d
guncebektas <guncebektas@gmail.com>
2017-11-26 21:29:19 +0300

    changes

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 /** 
- * autoCms
+ * AutoCms
  * 
  * A plug & play style admin panel which is working with autoForm.
  * 
@@ -9,7 +9,7 @@
  */
 Package.describe({
   name: 'guncebektas:autocms',
-  version: '1.1.40',
+  version: '1.1.41',
   // Brief, one-line summary of the package.
   summary: 'AutoCms is a simple solution to manage contents',
   // URL to the Git repository containing the source code for this package.
@@ -32,27 +32,27 @@ Package.onUse(function(api) {
     'kadira:flow-router@2.12.1',
     'kadira:blaze-layout@2.3.0',
     'arillo:flow-router-helpers@0.4.6',
-    'aldeed:template-extension@4.0.0',
+    'aldeed:template-extension@3.4.3 || 4.0.0',
     'aldeed:collection2@2.7.1',
     'aldeed:autoform@5.8.1 || 6.2.0',
     'aldeed:simple-schema@1.5.3',
     'aldeed:delete-button@2.0.0',
-    'elevatedevdesign:autoform-slingshot@0.0.8',
+    'elevatedevdesign:autoform-slingshot@0.0.5 || 0.0.8',
     'yogiben:autoform-file@0.4.2'
   ]);
   
-  api.use(["tap:i18n"], ["client", "server"]);
+  api.use(["tap:i18n@1.8.1"], ["client", "server"]);
 
   // api.use('ecmascript');
   
   api.use([
     'underscorestring:underscore.string@3.2.2',
-    'aldeed:template-extension@4.0.0',
+    'aldeed:template-extension@3.4.3 || 4.0.0',
     'aldeed:collection2@2.7.1',
     'aldeed:autoform@5.8.1 || 6.2.0',
     'aldeed:simple-schema@1.5.3',
     'aldeed:delete-button@2.0.0',
-    'elevatedevdesign:autoform-slingshot@0.0.8',
+    'elevatedevdesign:autoform-slingshot@0.0.5 || 0.0.8',
     'yogiben:autoform-file@0.4.2'
   ], ['client','server'], {weak: false, unordered: false});
 
